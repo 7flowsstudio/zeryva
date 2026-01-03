af9b43bbce22ef2268989eaacf80f345e5f84a35
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-03 16:33:23 +0200

    first-sett-about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/Sections/About/About";
 import Hero from "@/components/Sections/Hero/Hero";
 
 export default function Home() {
-	return (
-		<>
-			<Hero />
-		</>
-	);
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
 }
