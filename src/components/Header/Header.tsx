53c0656ad5d288462cf2827db122ddbf73b3c5d8
7flowsstudio <seven.flows.studio@gmail.com>
2026-01-08 17:11:20 +0100

    add icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import s from "./Header.module.css";
 import DescHeader from "./DescHeader/DescHeader";
 import MobHeader from "./MobHeader/MobHeader";
 
-const Header = () => {
-	const [isScrolled, setIsScrolled] = useState(false);
+type IsScroledProp = {
+	isScrolled?: boolean;
+};
 
-	useEffect(() => {
-		const scrollContainer = document.getElementById("app-scroll");
-		if (!scrollContainer) return;
-
-		const handleScroll = () => {
-			setIsScrolled(scrollContainer.scrollTop > 80);
-		};
-
-		scrollContainer.addEventListener("scroll", handleScroll);
-		handleScroll();
-
-		return () => scrollContainer.removeEventListener("scroll", handleScroll);
-	}, []);
-
+const Header = ({ isScrolled }: IsScroledProp) => {
 	return (
 		<div className={`${s.header} ${isScrolled ? s.scrolled : ""}`}>
 			<div className="container">
