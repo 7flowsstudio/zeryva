c161b946bbdea50cbeb469fab05d2839845e645b
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-19 17:34:11 +0200

    add-correction-admin-footer

--- src/components/Admin/AdminPage/OptionSide/Products/Products.tsx
+++ src/components/Admin/AdminPage/OptionSide/Products/Products.tsx
@@ -2,7 +2,14 @@
 import React, { useEffect, useState } from "react";
 import s from "./Products.module.css";
 import { Product, ProductWithId } from "../../../../../../utils/types";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../../../../../../firebaseConfig";
 
 type ProductsProps = {
@@ -25,17 +32,32 @@ const Products: React.FC<ProductsProps> = ({ onEdit }) => {
     fetchProducts();
   }, []);
 
+  const handleDelete = async (id: string) => {
+    await deleteDoc(doc(db, "products", id));
+    setProducts((prev) => prev.filter((p) => p.id !== id));
+  };
+
   return (
     <div className={s.cont}>
       <h2>Всі продукти</h2>
-      {products.map((product) => (
-        <div className={s.wrapp} key={product.id}>
-          <p className={s.name}>{product.title}</p>
-          <button className={s.btn} onClick={() => onEdit(product)}>
-            Редагувати
-          </button>
-        </div>
-      ))}
+      <div className={s.wrappCont}>
+        {products.map((product) => (
+          <div className={s.item} key={product.id}>
+            <p className={s.name}>{product.title}</p>
+            <div className={s.btnsWrapp}>
+              <button className={s.btn} onClick={() => onEdit(product)}>
+                Редагувати
+              </button>
+              <button
+                className={s.btnDel}
+                onClick={() => handleDelete(product.id)}
+              >
+                Видалити
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
