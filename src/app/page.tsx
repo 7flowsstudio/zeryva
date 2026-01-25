1582d83b2c1695d814460b1366f248dfd5ce50ac
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-25 20:01:30 +0200

    add-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import Call from "@/components/Sections/Call/Call";
-import About from "@/components/Sections/About/About";
+"use client";
+import dynamic from "next/dynamic";
+const Call = dynamic(() => import("@/components/Sections/Call/Call"), {
+  ssr: false,
+});
+// import Call from "@/components/Sections/Call/Call";
+// import About from "@/components/Sections/About/About";
+const About = dynamic(() => import("@/components/Sections/About/About"), {
+  ssr: false,
+});
 import Hero from "@/components/Sections/Hero/Hero";
-import Bestsellers from "@/components/Sections/Bestsellers/Bestsellers";
+// const Hero = dynamic(() => import("@/components/Sections/Hero/Hero"), {
+//   ssr: false,
+// });
+const Bestsellers = dynamic(
+  () => import("@/components/Sections/Bestsellers/Bestsellers"),
+  {
+    ssr: false,
+  },
+);
+// import Bestsellers from "@/components/Sections/Bestsellers/Bestsellers";
 
 export default function Home() {
   return (
