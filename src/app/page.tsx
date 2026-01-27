618536cb951fa2759944e9afc70264b900ee3b11
7flowsstudio <seven.flows.studio@gmail.com>
2026-01-27 22:07:08 +0100

    delete opacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
 "use client";
-import dynamic from "next/dynamic";
-const Call = dynamic(() => import("@/components/Sections/Call/Call"), {
-  ssr: false,
-});
-// import Call from "@/components/Sections/Call/Call";
-// import About from "@/components/Sections/About/About";
-const About = dynamic(() => import("@/components/Sections/About/About"), {
-  ssr: false,
-});
+// import dynamic from "next/dynamic";
+
 import Hero from "@/components/Sections/Hero/Hero";
+import Call from "@/components/Sections/Call/Call";
+import About from "@/components/Sections/About/About";
+import Bestsellers from "@/components/Sections/Bestsellers/Bestsellers";
 // const Hero = dynamic(() => import("@/components/Sections/Hero/Hero"), {
-//   ssr: false,
+// 	ssr: false,
+// });
+// const About = dynamic(() => import("@/components/Sections/About/About"), {
+// 	ssr: false,
+// });
+// const Bestsellers = dynamic(
+// 	() => import("@/components/Sections/Bestsellers/Bestsellers"),
+// 	{
+// 		ssr: false,
+// 	}
+// );
+// const Call = dynamic(() => import("@/components/Sections/Call/Call"), {
+// 	ssr: false,
 // });
 
-const Bestsellers = dynamic(
-  () => import("@/components/Sections/Bestsellers/Bestsellers"),
-  {
-    ssr: false,
-  },
-);
-// import Bestsellers from "@/components/Sections/Bestsellers/Bestsellers";
-
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <About />
-      <Bestsellers />
-      <Call />
-    </>
-  );
+	return (
+		<>
+			<Hero />
+			<About />
+			<Bestsellers />
+			<Call />
+		</>
+	);
 }
