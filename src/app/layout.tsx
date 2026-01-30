1bbf9fdedd975ff284c66c53b21bf7f1edb33812
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-30 15:04:03 +0200

    add-font-css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,28 +21,6 @@ const ptSans = localFont({
   display: "swap",
 });
 
-// const workSans = localFont({
-//   src: [
-//     {
-//       path: "../../public/fonts/WorkSans/WorkSansSemiBold.woff2",
-//       weight: "600",
-//       style: "normal",
-//     },
-//     {
-//       path: "../../public/fonts/WorkSans/WorkSansExtraBold.woff2",
-//       weight: "800",
-//       style: "normal",
-//     },
-//     {
-//       path: "../../public/fonts/WorkSans/WorkSansBlack.woff2",
-//       weight: "900",
-//       style: "normal",
-//     },
-//   ],
-//   variable: "--third-family",
-//   display: "swap",
-// });
-
 const workSans = localFont({
   src: "../../public/fonts/WorkSans/Inter-VariableFont_opsz,wght.ttf",
   variable: "--third-family",
