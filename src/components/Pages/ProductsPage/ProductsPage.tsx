4fd7c7678eb8c620a1e5da547b0eee1f3a06d0b1
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-30 15:41:13 +0200

    add-all

--- src/components/Pages/ProductsPage/ProductsPage.tsx
+++ src/components/Pages/ProductsPage/ProductsPage.tsx
@@ -4,20 +4,24 @@ import React, { Suspense } from "react";
 import s from "./ProductsPage.module.css";
 import Products from "./Products/Products";
 import BreadCrumbs from "@/components/Sections/UI/BreadCrumbs/BreadCrumbs";
-import TitleVideo from "./TitleVideo/TitleVideo";
+import dynamic from "next/dynamic";
+// import TitleVideo from "./TitleVideo/TitleVideo";
+const TitleVideo = dynamic(() => import("./TitleVideo/TitleVideo"), {
+  ssr: false,
+});
 
 const ProductsPage = () => {
-	return (
-		<div className={s.cont}>
-			<BreadCrumbs
-				crumbs={[{ label: "Головна", href: "/" }, { label: "Продукти" }]}
-			/>
-			<TitleVideo />
-			<Suspense fallback={<p>Завантаження продуктів...</p>}>
-				<Products />
-			</Suspense>
-		</div>
-	);
+  return (
+    <div className={s.cont}>
+      <BreadCrumbs
+        crumbs={[{ label: "Головна", href: "/" }, { label: "Продукти" }]}
+      />
+      <TitleVideo />
+      <Suspense fallback={<p>Завантаження продуктів...</p>}>
+        <Products />
+      </Suspense>
+    </div>
+  );
 };
 
 export default ProductsPage;
