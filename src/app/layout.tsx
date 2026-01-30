fa571ca68c8c24609156a4db1c1d2888d03debce
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-30 20:54:07 +0200

    add-woff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import localFont from "next/font/local";
 const ptSans = localFont({
   src: [
     {
-      path: "../../public/fonts/PTSansCaptions/PTSansCaption-Regular.ttf",
+      path: "../../public/fonts/PTSansCaptions/PTSansCaptionRegular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/PTSansCaptions/PTSansCaption-Bold.ttf",
+      path: "../../public/fonts/PTSansCaptions/PTSansCaptionBold.woff",
       weight: "700",
       style: "normal",
     },
@@ -20,12 +20,6 @@ const ptSans = localFont({
   variable: "--font-family",
   display: "swap",
 });
-
-// const workSans = localFont({
-//   src: "../../public/fonts/WorkSans/Inter-VariableFont_opsz,wght.ttf",
-//   variable: "--third-family",
-//   display: "swap",
-// });
 
 export const metadata: Metadata = {
   title: `ТОВ "Zeryva" - офіційний сайт`,
@@ -70,52 +64,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// import type { Metadata } from "next";
-// import "./globals.css";
-// import "./variables.css";
-// import { ClientLayout } from "./ClientLayout";
-
-// export const metadata: Metadata = {
-// 	title: `ТОВ "Zeryva" - офіційний сайт`,
-// 	description:
-// 		"Інокулянти, фунгіциди, мікродобрива та засоби захисту рослин — агрохімія для високих урожаїв",
-// 	metadataBase: new URL("http://zeryva.com.ua"),
-// 	alternates: {
-// 		canonical: "http://zeryva.com.ua",
-// 	},
-// 	openGraph: {
-// 		title: 'ТОВ "Zeryva" - офіційний сайт',
-// 		description:
-// 			"Інокулянти, фунгіциди, мікродобрива та засоби захисту рослин — агрохімія для високих урожаїв",
-// 		url: "http://zeryva.com.ua",
-// 		siteName: "Zeryva",
-// 		type: "website",
-// 		images: [
-// 			{
-// 				url: "/logo.svg",
-// 				width: 504,
-// 				height: 504,
-// 				alt: "Zeryva",
-// 			},
-// 		],
-// 	},
-// 	icons: {
-// 		icon: "/logo.svg",
-// 	},
-// };
-
-// export default function RootLayout({
-// 	children,
-// }: Readonly<{
-// 	children: React.ReactNode;
-// }>) {
-// 	return (
-// 		<html lang="uk">
-// 			<body id="app-scroll">
-// 				<ClientLayout>{children}</ClientLayout>
-// 				<div id="modal-root" />
-// 			</body>
-// 		</html>
-// 	);
-// }
