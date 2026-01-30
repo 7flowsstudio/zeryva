755c75f45a07e30fb7069c6ec7d3832aa6857ab4
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-01-30 17:43:22 +0200

    add-hook

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,12 +4,16 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { auth } from "../../../firebaseConfig";
 import AdminPage from "@/components/Admin/AdminPage/AdminPage";
+import { useFirebaseAuthIframe } from "../../components/hooks/useFirebaseAuthIframe";
 
 const Page = () => {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const authLoaded = useFirebaseAuthIframe(); // динамічно підвантажує iframe.js
 
   useEffect(() => {
+    if (!authLoaded) return; // чекаємо, поки скрипт завантажиться
+
     const unsub = onAuthStateChanged(auth, (user) => {
       if (!user) {
         router.replace("/login");
@@ -19,11 +23,34 @@ const Page = () => {
     });
 
     return () => unsub();
-  }, [router]);
+  }, [authLoaded, router]);
 
-  if (loading) return null;
+  if (loading || !authLoaded) return <p>Завантаження авторизації...</p>;
 
   return <AdminPage />;
 };
 
 export default Page;
+
+// const Page = () => {
+//   const [loading, setLoading] = useState(true);
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const unsub = onAuthStateChanged(auth, (user) => {
+//       if (!user) {
+//         router.replace("/login");
+//       } else {
+//         setLoading(false);
+//       }
+//     });
+
+//     return () => unsub();
+//   }, [router]);
+
+//   if (loading) return null;
+
+//   return <AdminPage />;
+// };
+
+// export default Page;
