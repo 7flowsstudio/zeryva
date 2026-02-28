57e63676df05d4ea55346514bcef2cc66a515201
7flowsstudio <seven.flows.studio@gmail.com>
2026-02-28 22:10:20 +0100

    add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 import "./variables.css";
 import { ClientLayout } from "./ClientLayout";
 import localFont from "next/font/local";
+import Script from "next/script";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 const ptSans = localFont({
 	src: [
@@ -77,7 +81,46 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="uk" className={`${workSans.variable} ${ptSans.variable}`}>
+			<head>
+				{GA_ID && (
+					<>
+						<Script
+							src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+							strategy="beforeInteractive"
+						/>
+						<Script id="google-analytics" strategy="beforeInteractive">
+							{`
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${GA_ID}');
+							`}
+						</Script>
+					</>
+				)}
+				{GTM_ID && (
+					<Script id="google-tag-manager" strategy="beforeInteractive">
+						{`
+							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','${GTM_ID}');
+						`}
+					</Script>
+				)}
+			</head>
 			<body id="app-scroll">
+				{GTM_ID && (
+					<noscript>
+						<iframe
+							src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+							height="0"
+							width="0"
+							style={{ display: "none", visibility: "hidden" }}
+						/>
+					</noscript>
+				)}
 				<ClientLayout>{children}</ClientLayout>
 				<div id="modal-root" />
 			</body>
