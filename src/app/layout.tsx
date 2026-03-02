43b4110d6518ec95c6982fe11b2ab3ca46f21422
7flowsstudio <seven.flows.studio@gmail.com>
2026-03-02 21:59:14 +0100

    add corect in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
 								window.dataLayer = window.dataLayer || [];
 								function gtag(){dataLayer.push(arguments);}
 								gtag('js', new Date());
-								gtag('config', '${GA_ID}', { cookie_domain: 'none' });
+								gtag('config', '${GA_ID}', { cookie_domain: 'zeryva.com.ua' });
 							`}
 						</Script>
 					</>
@@ -102,7 +102,6 @@ export default function RootLayout({
 					<Script id="google-tag-manager" strategy="beforeInteractive">
 						{`
 							window.dataLayer = window.dataLayer || [];
-							window.dataLayer.push({ cookie_domain: 'auto' });
 							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
