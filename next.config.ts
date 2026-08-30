e13011da15669fcae501b557fd5cdf28cbf7de8f
7flowsstudio <seven.flows.studio@gmail.com>
2026-08-30 17:56:18 +0200

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+	reactCompiler: true,
+
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
+
+	async redirects() {
+		return [
+			{
+				source: "/preparat-dlia-pidvyshchennia-rodiuchosti-gruntu",
+				destination: "/preparat-dlia-pidvyshchennia-rodiuchosti-gruntu-hleon",
+				permanent: true,
+			},
+			{
+				source: "/roduchist-gruntu",
+				destination: "/preparat-dlia-pidvyshchennia-rodiuchosti-gruntu-hleon",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
