9eef2a2755d15900022b0253ea87c2e50d7e9a19
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-09-24 15:55:33 +0300

    add-first-sett-categories

--- src/components/Sections/Categories/Categories.tsx
+++ src/components/Sections/Categories/Categories.tsx
@@ -1,12 +1,25 @@
 "use client";
 import React, { useState } from "react";
 import s from "./Categories.module.css";
+import { items } from "@/data/сategories/categories";
+import Link from "next/link";
+import Image from "next/image";
 
 const Categories = () => {
 	return (
-		<div>
+		<div className={`container ${s.categorCont}`}>
 			<h2>Категорії</h2>
-			<ul></ul>
+			<div className={s.gallery}>
+				{items.map((item) => (
+					<Link href={item.href} className={s.item} key={item.title}>
+						<Image src={item.image} alt={item.title} fill className={s.image} />
+
+						<div className={s.overlay} />
+
+						<span className={s.title}>{item.title}</span>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 };
